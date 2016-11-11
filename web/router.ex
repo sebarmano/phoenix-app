a0691e32e90a854beb0c6f54094dda3ecd62d859
Sebastian Armano <sebarmano@gmail.com>
2016-11-11 10:13:45 -0500

    Initial commit. Add user creation

---
+++ web/router.ex
@@ -0,0 +1,30 @@
+defmodule Rumbl.Router do
+  use Rumbl.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Rumbl do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+
+    # get "/users", UserController, :index
+    # get "/users/:id", UserController, :show
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Rumbl do
+  #   pipe_through :api
+  # end
+end
