b75669a1bf4add09af75e2f9f12ac016e59b1dc2
Sebastian Armano <sebarmano@gmail.com>
2017-03-12 16:26:39 -0400

    Add watch controller and views and missing tests

--- test/support/model_case.ex
+++ test/support/model_case.ex
@@ -20,7 +20,9 @@ defmodule Rumbl.ModelCase do
 
       import Ecto
       import Ecto.Changeset
-      import Ecto.Query
+      import Ecto.Query, only: [from: 1, from: 2]
+
+      import Rumbl.TestHelpers
       import Rumbl.ModelCase
     end
   end
@@ -57,8 +59,8 @@ defmodule Rumbl.ModelCase do
       iex> {:password, "is unsafe"} in changeset.errors
       true
   """
-  def errors_on(struct, data) do
-    struct.__struct__.changeset(struct, data)
+  def errors_on(model, data) do
+    model.__struct__.changeset(model, data)
     |> Ecto.Changeset.traverse_errors(&Rumbl.ErrorHelpers.translate_error/1)
     |> Enum.flat_map(fn {key, errors} -> for msg <- errors, do: {key, msg} end)
   end
--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,7 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    get "/watch/:id", WatchController, :show
 
     # get "/users", UserController, :index
     # get "/users/:id", UserController, :show
